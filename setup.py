a97f89e1c78f7904b3c2a95677bdfc5a3042b8de
Janice Kaur <59028457+JaniceKaur@users.noreply.github.com>
2022-02-27 22:26:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'TopsisJanice',         # How you named your package folder (MyLib)
   packages = ['TopsisJanice'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Topsis Package',   # Give a short description about your library
   author = 'Janice',                   # Type in your name
